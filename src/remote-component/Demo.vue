469d0e1417726d57c901ac089892ddc285994e71
lijiayuan <ljy@ljy,com>
2025-01-24 11:47:05 +0800

    feat: 移动端 px 适配

---
+++ src/remote-component/Demo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="demo">
+    <h1>Hello {{ name }}</h1>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  name: {
+    type: String,
+    required: true,
+    default: 'World',
+  },
+}>()
+</script>
+<style scoped>
+.demo {
+  width: 375px;
+  background-color: red;
+}
+h1 {
+  /* color: red; */
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import postcsspxtoviewport from 'postcss-px-to-viewport'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,4 +18,24 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    postcss: {
+      plugins: [
+        postcsspxtoviewport({
+          unitToConvert: 'px',
+          viewportWidth: 750,
+          unitPrecision: 5,
+          propList: ['*'],
+          viewportUnit: 'vw',
+          fontViewportUnit: 'vw',
+          selectorBlackList: [],
+          minPixelValue: 1,
+          mediaQuery: false,
+          replace: true,
+          exclude: undefined,
+          landscape: false,
+        })
+      ]
+    }
+  }
 })
